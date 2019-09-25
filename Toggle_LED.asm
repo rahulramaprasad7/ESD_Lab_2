3220364694958e4a37fe1122287f7d3f18582412
rahulramaprasad7 <rara3459@colorado.edu>
2019-09-24 22:08:33 -0600

    Output Verified and toggling

---
+++ Toggle_LED.asm
@@ -0,0 +1,33 @@
+	     ORG 00h
+
+BEGIN:	 MOV P1, #00h 	    ;Clearing Port1
+		 MOV TMOD, #00h   	;Using Timer 0 in mode 0
+	     MOV R1, #0Ah       ;Storing the count for 50ms delay
+	     CLR TR0
+	     CLR TF0
+	     MOV IE, #82h       ;Enabling interrupt for Timer0
+	  
+LOOP: 	 AJMP LED      	    ;Continue to toggle the LED 
+      	  
+	     ORG 0Bh
+	     LJMP ISR_TMR0
+
+LED: 	 CPL P1.0          ;Switching on LED on P1.1
+         ACALL DELAY        ;Keeping the LED ON for 50ms
+	     DJNZ R1, LED    	;Execute 50ms delay 10 times
+	     MOV R1, #0Ah       ;Restoring the count value
+	  	  
+DELAY:   MOV TH0, #4Bh      ;Upper nibble value for 50ms delay
+	     MOV TL0, #0FDh     ;Lower nibble value for 50ms delay
+	     SETB TR0           ;Starting Timer0
+CHECK:   JNB TF0, CHECK     ;Waiting for TF0 flag to get set and trigger an interrupt
+       
+	   
+ISR_TMR0:SETB P1.1          ;Setting the port pin P1.1
+		 CLR P1.1           ;Clearing the port pin P1.1 
+		 CLR TR0
+		 CLR TF0
+		 RETI 
+
+
+ 
