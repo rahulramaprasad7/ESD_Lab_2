d4fccaf5a5cfef12df0b5ea55ca50b5a902b7e3d
rahulramaprasad7 <rara3459@colorado.edu>
2019-09-26 05:47:30 -0600

    Errors Fixed

--- Interrupt_LED.asm
+++ Interrupt_LED.asm
@@ -1,37 +1,34 @@
 	     ORG 00h
 
-BEGIN:	 MOV P1, #00h 	    ;Clearing Port1
-		 MOV TMOD, #01h   	;Using Timer 0 in mode 0
-	     MOV R1, #0Ah       ;Storing the count for 50ms delay
-	     CLR TR0
-	     CLR TF0
-	     MOV IE, #82h       ;Enabling interrupt for Timer0
- 
-	     ORG 0Bh
-	     LJMP ISR_TMR0
+BEGIN:	 MOV TMOD, #01h   ;Using Timer 0 in mode 1         
+         SJMP INIT        ;Jumping to the Initialization  
+         
+         ORG 000Bh        ;Vector address of Timer0 Interrupt
+         ACALL ISR        ;Calling the ISR sub-routine
+         RETI             ;Returning from the ISR 
+         
+         ORG 0040h
+INIT:    MOV R1, #0Ah     ;Counting 50ms Delay 10 times
+         MOV TH0, #4Bh    ;Upper Nibble value for counting 50ms
+         MOV TL0, #0FCh   ;Lower Nibble value for counting 50ms  
+	     MOV IE, #82h     ;Enabling Global and Timer 0 interrupt
 
-LEDON: 	 SETB P1.0          ;Switching on LED on P1.1
-         ACALL DELAY        ;Keeping the LED ON for 50ms
-	     DJNZ R1, LEDON  	;Execute 50ms delay 10 times
-	     MOV R1, #0Ah       ;Restoring the count value
-	  
-LEDOFF:  CLR P1.0           ;Switching off LED on P1.1
-         ACALL DELAY        ;Keeping the LED OFF for 50ms
-	     DJNZ R1, LEDOFF    ;Execute 50ms delay 10 times
-		 MOV R1, #0Ah       ;Restoring the count value
-		 AJMP LEDON
-	  
-DELAY:   MOV TH0, #4Bh      ;Upper nibble value for 50ms delay
-	     MOV TL0, #0FCh     ;Lower nibble value for 50ms delay
-	     SETB TR0           ;Starting Timer0
-CHECK:   JNB TF0, CHECK     ;Waiting for TF0 flag to get set and trigger an interrupt
-       
+MAIN:    CPL P1.1         ;Toggling the LED
+
+TIMER:   SETB TR0         ;Starting Timer0
+
+CHECK:   JNB TF0, CHECK   ;Waiting for TF flag to overflow and interrupt
+         DJNZ R1, TIMER   ;
+         MOV R1, #0Ah     ;Restore the counter value  
+         AJMP MAIN        ;Jump to toggle the led
+
+         ORG 0060h
+ISR :    SETB P1.2        ;Set the unused port pin before exiting ISR
+         MOV TH0, #4Bh    ;Restore Upper Nibble count for next iteration
+         MOV TL0, #0FCh   ;Restore Lower Nibble count for next iteration
+         CLR P1.2         ;Clear the unused port pin before exiting ISR
+         RET              ;Return from the sub-routine
+         
+      
 	   
-ISR_TMR0:SETB P1.1          ;Setting the port pin P1.1
-		  CLR TR0
-		 CLR TF0
-		 CLR P1.1           ;Clearing the port pin P1.1 
-		 RETI 
 
-
- 
